ec61e2dc2a7885d2fef261f44e277c076e057f9b
Felipe <felipe.cadavid3@gmail.com>
2021-06-07 21:47:06 -0500

    Third commit - Responsive

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-screen.orientation.lock();
+screen.orientation.lock("portrait-primary");
 
 let piecesArr = new Array(16);
 let currentArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'];
